5c2a765423a7c812e1c043c6ce7b024614cec02a
echen341 <lightning10pok@gmail.com>
2023-01-02 17:16:42 -0800

    added curriculum page, css doesnt work idk why

--- pages/curriculum/index.js
+++ pages/curriculum/index.js
@@ -1,5 +1,75 @@
+import styles from './index.module.css';
 export default function CurriculumPage () {
     return (
-        <div> Curriculum </div>
+        <div>
+            <p id = "med1" className= {styles.med}>
+            Faith Hope and Love Preschool program is designed for children ages 2 - 6 year-old. Our program promotes learning through socialization and engaging activities to allow children to work together and learn from each other. Our goal is to provide a safe and nurturing environment and to encompass all aspects of learning to help our children to learn and grow into well rounded individuals.
+            <br/>
+            <br/>
+            </p>
+
+            <p id = "big2" className= {styles.big}>
+            Our curriculum activities include:
+            </p>
+
+            <p id = "small3" className={styles.small}>
+            <br/>
+            Small class size and low student-teacher ratio (6:1), which encourages student engagement
+            <br/>
+            Daily Chinese and English circle time
+            <br/>
+            Art, music and movement activities
+            <br/>
+            Activities for developing gross motor and fine motor skills
+            <br/>
+            Nurturing environment for developing social skills with peers and adults
+            <br/>
+            Introduction to basic counting, writing, and recognizing and counting numerals
+            <br/>
+            Introduction to simple words in both Chinese and English languages 
+            <br/>
+            Introduction of days of the week
+            <br/>
+            Different monthly themes
+            <br/>
+            Story time
+            <br/>
+            Other indoor activities.
+            <br/>
+            <br/>
+            </p>
+
+            <p id = "med4" className= {styles.med}>
+            At our school, the curriculum is carefully crafted with a mix of Montessori, Froebel, and Reggio. Our goal is to introduce children to many types of activities so they are able to pick and choose their own interests, building their confidence and personality as an individual, as well as strengthen their relationship and knowledge about the love of God. 
+            <br/>
+            <br/>
+            </p>
+            
+            <p id = "big5" className= {styles.big}>
+            We aim to encourage the child’s growth in following major areas:
+            </p>
+
+            <p id = "small6" className={styles.small}>
+            <br/>
+            Social and Emotional development – help the child to deal with circumstances and others in a positive, helpful, and constructive way
+            <br/>
+            Intellectual development – help the child to discover the satisfaction in accomplishing meaningful tasks and learning new skills
+            <br/>
+            Spiritual development – help the child to understand God’s encompassing love
+            <br/>
+            Physical development - help the child to understand nature and to care for the world we live in.
+            <br/>
+            </p>
+
+            <p id = "big7" className= {styles.big}>
+            Outdoor Play
+            </p>
+
+            <p id = "med8" className= {styles.med}>
+            <br/>
+            <br/>
+            Faith Hope & Love Preschool is blessed with a very large campus and spacious play area, with a varieties of play structures and outdoor activities that are set up for the children to play and explore. This area serves as a place for the children to learn and engage the world around them while also serves as a place to relax. Having fun and running around will allow the children to communicate their feelings and learn social behaviors with their peers. The playground is the place where they will learn many important life lessons and values of sharing, empathy, kindness etc.
+            </p>
+        </div>
     );
 }

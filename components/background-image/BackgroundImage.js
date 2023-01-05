bf508c4bc2a43c57b207a8cbd289f743f67de9a7
Alexander Lee <alexleeyt8888@gmail.com>
2023-01-04 22:02:40 -0800

    fixed errors

--- components/background-image/BackgroundImage.js
+++ components/background-image/BackgroundImage.js
@@ -1,19 +1,28 @@
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 import styles from './BackgroundImage.module.css'
 
 export default function BackgroundImage(props) {
-    return (
-        <div>
-            <Image className={styles.fixedbgi}
-                src={props.url}
-                alt="backgroundImage"
-                width="0"
-                height="0"
-            />
 
-            <main>
-                {props.children}
-            </main>
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        containerRef.current.style.width = `100vw`;
+        containerRef.current.style.height = `100vh`;
+    }, []);
+
+
+    return (
+
+        <div ref={containerRef} style={{
+            backgroundImage: props.url,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width:'100%',
+            height:'100%',
+            
+        }}>
+            <Image src={props.url} alt="background image" width='100vw' height='100vh' />
         </div>
 
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery/dist/jquery.slim.js';
+// import 'popper.js/dist/umd/popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.js';
+// boostrap stuff
+
+
 import '../styles/globals.css'
+
 import Layout from '../components/layout/Layout';
 
 

6a2866004afdd188ca73806eb950fd1fb0fa0c16
Alexander Lee <alexleeyt8888@gmail.com>
2023-01-04 22:14:28 -0800

    fixed stupid errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery/dist/jquery.slim.js';
-import 'popper.js/dist/umd/popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery/dist/jquery.slim.js';
+// import 'popper.js/dist/umd/popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout';

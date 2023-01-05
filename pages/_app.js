f348e19c63617e796211ece47e831e67653217d9
Alexander Lee <alexleeyt8888@gmail.com>
2023-01-04 18:49:08 -0800

    added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
+import '../styles/customTheme.scss'
+
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout';
 
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  ); 
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+
+
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 
 export default function HomePage() {
   return (
-    <div>
-      <p>
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-        home page
-      </p>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          1 of 2
+        </div>
+        <div className="col">
+          2 of 2
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          1 of 3
+        </div>
+        <div className="col">
+          2 of 3
+        </div>
+        <div className="col">
+          3 of 3
+        </div>
+      </div>
     </div>
-      
+
 
   );
 

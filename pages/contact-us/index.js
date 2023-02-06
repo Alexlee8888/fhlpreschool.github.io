b481acf890560fee4b57ecf0f6a2f4ec3b380537
Alexander Lee <alexleeyt8888@gmail.com>
2023-02-05 22:03:57 -0800

    finished contact us page

--- pages/contact-us/index.js
+++ pages/contact-us/index.js
@@ -1,10 +1,12 @@
-import { Button, Container, FormControl, FormErrorMessage, FormLabel, Heading, Input, Textarea } from '@chakra-ui/react';
+import { Button, Center, Container, FormControl, FormErrorMessage, FormLabel, Heading, Input, Text, Textarea, useToast } from '@chakra-ui/react';
 import styles from './index.module.css';
 import { useState } from 'react';
 import { sendContactForm } from '../../lib/api';
 import Footer from '../../components/layout/Footer';
 import { ChakraProvider } from '@chakra-ui/react'
 import bg from '../../public/Contact.png'
+import ParallaxLayout from '../../components/parallax/ParallaxLayout';
+import Image from 'next/image';
 
 
 
@@ -13,17 +15,35 @@ const initValues = { name: "", email: "", message: "", }
 const initState = { values: initValues }
 
 export default function ContactPage() {
+    const toast = useToast();
     const [state, setState] = useState(initState);
     const [touch, setTouch] = useState({});
 
-    const { values, isLoading } = state;
+    const { values, isLoading, error } = state;
 
     const onSubmit = async () => {
         setState((prev) => ({
             ...prev,
             isLoading: true
         }));
-        await sendContactForm(values);
+        try {
+            await sendContactForm(values);
+            setTouch({});
+            setState(initState);
+            toast({
+                title: "Success!",
+                status: "success",
+                duration: 2000,
+                position: "top"
+            })
+        } catch (error) {
+            setState((prev) => ({
+                ...prev,
+                isLoading: false,
+                error: error.message
+            }));
+        }
+
     }
 
     const onBlur = ({ target }) => setTouch((prev) => ({
@@ -39,66 +59,78 @@ export default function ContactPage() {
     }))
 
     return (
-        <ChakraProvider>
-            <Container className={styles.container}>
-                <Heading className={styles.title}>Contact Us</Heading>
+        <div className={styles.div}>
+            <ChakraProvider>
+                <Container className={styles.container}>
+                    <Heading className={styles.title}>Contact Us</Heading>
 
 
-                <FormControl isRequired isInvalid={touch.name && !values.name} className={styles.formcontrol}>
-                    <FormLabel>Name</FormLabel>
-                    <Input
-                        type='text'
-                        name="name"
-                        errorBorderColor='red.300'
-                        value={values.name}
-                        onChange={handleChange}
-                        onBlur={onBlur}
+                    {error && (
+                        <header className={styles.errormessage}>
+                            {error}
+                        </header>
+                    )}
 
-                    />
-                    <FormErrorMessage>Required</FormErrorMessage>
-                </FormControl>
+                    <FormControl isRequired isInvalid={touch.name && !values.name} className={styles.formcontrol}>
+                        <FormLabel>Name</FormLabel>
+                        <Input
+                            type='text'
+                            name="name"
+                            errorBorderColor='red.300'
+                            value={values.name}
+                            onChange={handleChange}
+                            onBlur={onBlur}
 
-                <FormControl isRequired isInvalid={touch.email && !values.email} className={styles.formcontrol}>
-                    <FormLabel>Email</FormLabel>
-                    <Input
-                        type='email'
-                        name="email"
-                        errorBorderColor='red.300'
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={onBlur}
-                    />
-                    <FormErrorMessage>Required</FormErrorMessage>
-                </FormControl>
+                        />
+                        <FormErrorMessage>Required</FormErrorMessage>
+                    </FormControl>
 
-                <FormControl isRequired isInvalid={touch.message && !values.message} className={styles.formcontrol}>
-                    <FormLabel>Message</FormLabel>
-                    <Textarea
-                        type='text'
-                        name="message"
-                        errorBorderColor='red.300'
-                        rows={5}
-                        value={values.message}
-                        onChange={handleChange}
-                        onBlur={onBlur}
-                    />
-                    <FormErrorMessage>Required</FormErrorMessage>
+                    <FormControl isRequired isInvalid={touch.email && !values.email} className={styles.formcontrol}>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            type='email'
+                            name="email"
+                            errorBorderColor='red.300'
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={onBlur}
+                        />
+                        <FormErrorMessage>Required</FormErrorMessage>
+                    </FormControl>
 
-                </FormControl>
+                    <FormControl isRequired isInvalid={touch.message && !values.message} className={styles.formcontrol}>
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                            type='text'
+                            name="message"
+                            errorBorderColor='red.300'
+                            rows={5}
+                            value={values.message}
+                            onChange={handleChange}
+                            onBlur={onBlur}
+                        />
+                        <FormErrorMessage>Required</FormErrorMessage>
 
-                <Button
-                    className={styles.button}
-                    variant="outline"
-                    colorScheme='blue'
-                    isLoading={isLoading}
-                    isDisabled={!values.name || !values.email || !values.message}
-                    onClick={onSubmit}
-                >
-                    Submit
-                </Button>
-            </Container>
-            <Footer />
-        </ChakraProvider>
+                    </FormControl>
+
+                    <Center>
+                        <Button
+                            className={styles.button}
+                            alignSelf="center"
+                            variant="outline"
+                            colorScheme='blue'
+                            isLoading={isLoading}
+                            isDisabled={!values.name || !values.email || !values.message}
+                            onClick={onSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Center>
+
+                </Container>
+                <Footer />
+            </ChakraProvider>
+        </div>
 
     );
 }

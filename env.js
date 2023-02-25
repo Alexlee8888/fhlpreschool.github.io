f3fe3c503df30e9081c2978883d664542fd4adb5
Alexlee8888 <alexleeyt8888@gmail.com>
2023-02-25 10:07:03 -0800

    test

--- env.js
+++ env.js
@@ -1,5 +1,9 @@
 module.exports = {
-    EMAIL : 'preschool@fhlschool.org',
-    EMAIL_PASS : 'tbvgvoxlnyvxkowt'
+    EMAIL: 'alexleeyt8888@gmail.com',
+    EMAIL_PASS: 'izxlewpsrhftknuk',
+
+
+    // EMAIL : 'preschool@fhlschool.org',
+    // EMAIL_PASS : 'tbvgvoxlnyvxkowt'
 }
 
